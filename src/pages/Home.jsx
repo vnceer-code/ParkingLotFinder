22a8e1177a2c65cf7d76f950b1c724c422bedee5
vincy <vnceer@gmail.com>
2026-03-06 21:57:35 +0530

    added setParkingLotsmin home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { use } from 'react'
 import MapView from '../components/MapView'
+import { parkingLots } from '../data/parkingData';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { setParkingLots } from '../features/parkingSlice';
 
 const Home = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(setParkingLots(parkingLots));
+    }, [])
     return (
         <div>
             <h1>Parking Finder</h1>
