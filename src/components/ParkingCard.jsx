73580fd15bf726140801c08e94d684f2735447c1
vincy <vnceer@gmail.com>
2026-03-11 20:52:07 +0530

    utils folder and slotUtils creted

--- src/components/ParkingCard.jsx
+++ src/components/ParkingCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { setSelectedParking } from "../features/parkingSlice";
+import { getAvailableSlots } from "../utils/slotUtils";
 
 const ParkingCard = () => {
 
@@ -21,23 +22,39 @@ const ParkingCard = () => {
 
     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
 
-      {parkingLots.map((lot) => (
-        <div
-          key={lot.id}
-          onClick={() => handleClick(lot)}
-          className="border rounded-lg p-4 shadow hover:shadow-lg cursor-pointer bg-white"
-        >
+      {parkingLots.map((lot) => {
 
-          <h3 className="text-lg font-semibold">
-            {lot.name}
-          </h3>
+        const availableSlots = getAvailableSlots(lot.id)
 
-          <p className="text-gray-600">
-            Total Slots: {lot.totalslots}
-          </p>
+        return (
+          <div
+            key={lot.id}
+            onClick={() => handleClick(lot)}
+            className="border rounded-lg p-4 shadow hover:shadow-lg  cursor-pointer bg-white transition"
+          >
 
-        </div>
-      ))}
+            <h3 className="text-lg font-semibold">
+              {lot.name}
+            </h3>
+
+            <p className="text-gray-600">
+              Total Slots: {lot.totalslots}
+            </p>
+
+            <p className="text-gray-600">
+              Available Slots: {availableSlots}
+            </p>
+
+          </div>
+        )
+
+      }
+
+
+
+
+
+      )}
 
     </div>
 
---
+++ src/utils/slotUtils.js
@@ -0,0 +1,11 @@
+export const getAvailableSlots = (parkingId) => {
+
+const stored = JSON.parse(localStorage.getItem("parkingSlots")) || {}
+
+const slots = stored[parkingId] || []
+
+return slots.filter(
+(slot) => slot.status === "available"
+).length
+
+}
